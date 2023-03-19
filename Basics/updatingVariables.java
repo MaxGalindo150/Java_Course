6127c5016cc7853c38e66c38243ef6b0f574060b
Maximiliano <maximilianogalindo7@gmail.com>
2023-03-19 16:58:27 -0600

    first commit

---
+++ Basics/updatingVariables.java
@@ -0,0 +1,11 @@
+public class updatingVariables{
+    public static void main(String[] arg){
+        int salary = 1000;
+
+        //bonus $200
+        salary = salary + 200;
+
+        //pension $50
+        salary = salary - 50;
+    }
+}
