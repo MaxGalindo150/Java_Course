97cd5f792dac65d0050de8023692f91a07bc1bf5
Maximiliano <maximilianogalindo7@gmail.com>
2023-03-20 23:07:44 -0600

    updating variables

--- Basics/updatingVariables.java
+++ Basics/updatingVariables.java
@@ -7,5 +7,17 @@ public class updatingVariables{
 
         //pension $50
         salary = salary - 50;
+
+        //extra hours $30 p/h
+        //food $45
+
+        salary = salary + (30*2) - 45;
+        System.out.println(salary);
+
+        //updating strings
+        String employeeName = "Max Galindo";
+        employeeName = employeeName + " Hernandez";
+
+        System.out.println(employeeName);
     }
 }
