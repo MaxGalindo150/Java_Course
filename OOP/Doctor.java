b355426cb347846a6bf11d87cb9641528d292237
Maximiliano <maximilianogalindo7@gmail.com>
2023-04-09 01:32:51 -0600

    nested classes

--- OOP/Doctor.java
+++ OOP/Doctor.java
@@ -1,12 +1,20 @@
 package OOP;
 
+import java.sql.Date;
+import java.util.ArrayList;
+
 public class Doctor {
     
     //Atributtes
     static int id = 0; //Autoincrement
-    String name,
-           email,
-           speciality;
+    private String name,
+                   email,
+                   speciality;
+
+    
+
+    
+    
 
     Doctor(String name, String speciality){
         this.name = name;
@@ -23,5 +31,54 @@ public class Doctor {
     public void showId(){
         System.out.println("ID Doctor: " + id);
     }
+
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time){
+        availableAppointments.add( new Doctor.AvailableAppointment(date, time))
+    }
     
+    public ArrayList<AvailableAppointment> getAvailableAppointments({
+        return availableAppointments;
+    }
+
+    public static class AvailableAppointment{
+        private int id;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time){
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return this.id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+           return this.date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return this.time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+
+        
+        
+        
+
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Java_Course
-This repository stores some programs made to learn the programming language Java. From the basics to learning object-oriented programming.
+This repository stores some programs made to learn the programming language Java. From the basics to object-oriented programming.
